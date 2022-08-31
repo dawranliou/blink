c2af7d72d115ca19913fafeb2e8caaeb173f13e1
Daw-Ran Liou <hi@dawranliou.com>
2022-08-31 17:27:41 +0800

    Make tile and player instances rather than just sprite entities

--- src/engine/tile.lisp
+++ src/engine/tile.lisp
@@ -1,14 +1,17 @@
 (in-package #:blink)
 
-(defclass tile (sprite) ())
+(defclass tile (sprite)
+  ((wallp :initarg :wallp :accessor wallp :initform nil)))
 
-(defun make-tile (tile-id x y)
-  (make-sprite *bg-tex*
-               (sdl2:make-rect (* tile-id 16) 0 16 16)
-               x y
-               +sprite-size+
-               +sprite-size+
-               :color +gray-50+))
+(defun make-tile (tile-id x y &key wallp)
+  (make-instance 'tile
+                 :tex *bg-tex*
+                 :rect (sdl2:make-rect (* tile-id 16) 0 16 16)
+                 :x x :y y
+                 :w +sprite-size+
+                 :h +sprite-size+
+                 :color +gray-50+
+                 :wallp wallp))
 
 ;; (make-tile 2 64 64)
 
@@ -18,7 +21,7 @@
         do (loop for tile-id in row
                  for x = 0 then (incf x +sprite-size+)
                  when (not (zerop tile-id))
-                   do (add-to-scene scene (make-tile tile-id x y)))))
+                   do (add-to-scene scene (make-tile tile-id x y :wallp t)))))
 
 ;; (load-room *room*)
 ;; (remove-all-entities-from-scene (scene *window*))
