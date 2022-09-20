5edfc7982765f2c3e699720ccefdb00dc7c0d589
Daw-Ran Liou <hi@dawranliou.com>
2022-09-20 12:48:39 +0800

    Bundle XQuartz deps on Mac; these are not shipped with Mac

--- release.lisp
+++ release.lisp
@@ -1,4 +1,7 @@
-(ql:quickload '("deploy" "cffi-libffi" "cl-glut" "cl-opengl"))
+(ql:quickload '("deploy"
+                "cffi-libffi"
+                "cl-glut"
+                "cl-opengl"))
 
 ;; (sb-ext:save-lisp-and-die "blink"
 ;;                           :executable t
@@ -6,28 +9,55 @@
 ;;                           :toplevel 'blink::main)
 
 #+darwin
-(deploy:define-library cffi::libffi
-  :path "/usr/local/opt/libffi/lib/libffi.dylib")
+(prog1 'darwin
+  (deploy:define-library cffi::libffi
+    :path "/usr/local/opt/libffi/lib/libffi.dylib")
 
-#+darwin
-(deploy:define-library deploy::libz
-  :path "/usr/local/opt/zlib/lib/libz.dylib")
+  (deploy:define-library deploy::libz
+    :path "/usr/local/opt/zlib/lib/libz.dylib")
 
-#+darwin
-(deploy:define-library cl-glut::glut
-  :path "/opt/X11/lib/libglut.dylib")
+  (deploy:define-library cl-glut::glut
+    :path "/opt/X11/lib/libglut.dylib")
 
-#+darwin
-(deploy:define-library cl-opengl-bindings::opengl
-  :path "/opt/X11/lib/libgl.dylib")
+  (deploy:define-library cl-opengl-bindings::opengl
+    :path "/opt/X11/lib/libgl.dylib")
 
-#+darwin
-(deploy::define-hook (:build sort-foreign-libraries) ()
-  ;; Pretend to sort the list but this is just to ensure LIBSDL2 is loaded
-  ;; before LIBSDL2-TTF and LIBSDL2-IMAGE
-  (let ((sorted-libraries (reverse deploy::*foreign-libraries-to-reload*)))
-    (setf deploy::*foreign-libraries-to-reload* sorted-libraries)
-    (deploy::status 1 "Sorted libraries: ~A" sorted-libraries)))
+  (cffi:define-foreign-library libX11
+    (T (:default "libX11")))
+  (deploy:define-library libX11
+    :path "/opt/X11/lib/libX11.dylib")
+
+  (cffi:define-foreign-library libX11-xcb
+    (T (:default "libX11-xcb")))
+  (deploy:define-library libX11-xcb
+    :path "/opt/X11/lib/libX11-xcb.dylib")
+
+  (cffi:define-foreign-library libXext
+    (T (:default "libXext")))
+  (deploy:define-library libXext
+    :path "/opt/X11/lib/libXext.dylib")
+
+  (cffi:define-foreign-library libglapi
+    (T (:default "libglapi")))
+  (deploy:define-library libglapi
+    :path "/opt/X11/lib/libglapi.dylib")
+
+  (cffi:define-foreign-library libxcb
+    (T (:default "libxcb")))
+  (deploy:define-library libxcb
+    :path "/opt/X11/lib/libxcb.dylib")
+
+  (cffi:define-foreign-library libxcb-glx
+    (T (:default "libxcb-glx")))
+  (deploy:define-library libxcb-glx
+    :path "/opt/X11/lib/libxcb-glx.dylib")
+
+  (deploy::define-hook (:build sort-foreign-libraries) ()
+    ;; Pretend to sort the list but this is just to ensure LIBSDL2 is loaded
+    ;; before LIBSDL2-TTF and LIBSDL2-IMAGE
+    (let ((sorted-libraries (reverse deploy::*foreign-libraries-to-reload*)))
+      (setf deploy::*foreign-libraries-to-reload* sorted-libraries)
+      (deploy::status 1 "Sorted libraries: ~A" sorted-libraries))))
 
 (deploy:define-resource-directory assets "assets/")
 
